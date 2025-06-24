3de0f69646f8ccec50fbcb2518128703f80b5c8f
Mwaiko <komomwai2005@gmail.com>
2025-06-24 17:40:19 +0300

    Update Priority&aging.py

--- Priority&aging.py
+++ Priority&aging.py
@@ -1,14 +1,5 @@
-import threading
-import time
-class PrintQueueManager:
-    def __init__(self, capacity=10, aging_interval=5, expiry_time=20):
-        self.capacity = capacity
-        self.queue = []
-        self.lock = threading.Lock()
-        self.current_time = 0
-        self.aging_interval = aging_interval
-        self.expiry_time = expiry_time
-    def apply_priority_aging(self):
+
+def apply_priority_aging(self):
         self.time_since_last_aging += 1
         
         if self.time_since_last_aging >= self.aging_interval:
@@ -20,5 +11,5 @@ class PrintQueueManager:
             
         self.queue.sort(key=lambda x: (x['priority'], x['waiting_time']))
     
-    def _sort_queue_by_priority(self):
+def _sort_queue_by_priority(self):
         self.queue.sort(key=lambda x: (x['priority'], x['waiting_time']))
